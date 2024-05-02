a44e2ad71715bc7bf5c3def23dd054692beb2392
Alexey <alexey.yus@mail.ru>
2024-05-02 10:08:23 +0300

    25 setting up storybook & decorators & story cases for components

---
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import MainPage from './MainPage';
+
+export default {
+    title: 'page/MainPage',
+    component: MainPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        to: '/',
+    },
+} as ComponentMeta<typeof MainPage>;
+
+const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
