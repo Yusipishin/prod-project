3cea7ae30ab6637d76cffd1e6c55042030882a3e
Alexey <alexey.yus@mail.ru>
2024-05-11 15:34:00 +0300

    30 redux-toolkit & entity & tests

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,10 @@
+import type { CounterSchema } from './model/types/CounterSchema';
+import { counterActions, counterReducer } from './model/slice/counterSlice';
+import { Counter } from './ui/Counter';
+
+export {
+    counterReducer,
+    counterActions,
+    CounterSchema,
+    Counter,
+};
