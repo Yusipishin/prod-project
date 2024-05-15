f9205c5363775b83ad6a3220fe8dabf5175dbfd4
Alexey <alexey.yus@mail.ru>
2024-05-15 20:28:29 +0300

    32 custom input & authorization window & lazy modal

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
+
+    const [value, setValue] = useState('');
+
+    const onChange = (value: string) => {
+        setValue(value);
+    };
 
     return (
         <div>
