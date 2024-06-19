9a0c3f7d2d69cb590cb2653fc434f2eb7af9435f
Alexey <alexey.yus@mail.ru>
2024-06-19 20:22:05 +0300

    49 entity article & skeleton loader

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
@@ -10,6 +11,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     const {
         className,
     } = props;
+    const { t } = useTranslation();
 
     return (
         <div className={classNames(cls.ArticlesPage, {}, [className])}>
