744f488cf8eea952419b83729f0a05f46c7091af
Alexey <alexey.yus@mail.ru>
2024-08-03 13:41:20 +0300

    77 CircularDependency

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -2,18 +2,6 @@ export {
     ArticleDetailsPageAsync as ArticleDetailsPage,
 } from './ui/ArticleDetailsPage/ArticleDetailsPage.async';
 
-export type {
-    ArticleDetailsCommentsSchema,
-} from './model/types/ArticleDetailsCommentsSchema';
-
-export type {
-    ArticleDetailsRecommendationsSchema,
-} from './model/types/ArticleDetailsRecommendationsSchema';
-
-export type {
-    ArticleDetailsPageSchema,
-} from './model/types';
-
-export {
-    getCanEditArticle,
-} from './model/selectors/article';
+export type { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
+export type { ArticleDetailsRecommendationsSchema } from './model/types/ArticleDetailsRecommendationsSchema';
+export type { ArticleDetailsPageSchema } from './model/types';
