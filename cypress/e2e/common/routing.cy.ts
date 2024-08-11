1804f5fd7d3369068cc0d5a6ecf5a47f8059fe26
Alexey <alexey.yus@mail.ru>
2024-08-11 14:52:55 +0300

    113 introduction to e2e testing with Cypress & router tests

---
+++ cypress/e2e/common/routing.cy.ts
@@ -0,0 +1,32 @@
+import { selectByTestId } from '../../helpers/selectByTestId';
+
+describe('Роутинг', () => {
+    describe('Пользователь не авторизован', () => {
+        it('Переход на главную страницу', () => {
+            cy.visit('/');
+            cy.get(selectByTestId('MainPage')).should('exist');
+        });
+        it('Переход на страницу профиля редиректит на главную', () => {
+            cy.visit('/profile/1');
+            cy.get(selectByTestId('MainPage')).should('exist');
+        });
+        it('Переход открывает несуществующую страницу', () => {
+            cy.visit('/rareferher');
+            cy.get(selectByTestId('NotFoundPage')).should('exist');
+        });
+    });
+
+    describe('Пользователь авторизован', () => {
+        beforeEach(() => {
+            cy.login();
+        });
+        it('Переход открывает страницу профиля', () => {
+            cy.visit('/profile/1');
+            cy.get(selectByTestId('ProfilePage')).should('exist');
+        });
+        it('Переход открывает страницу со списком статей', () => {
+            cy.visit('/articles');
+            cy.get(selectByTestId('ArticlesPage')).should('exist');
+        });
+    });
+});
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -56,6 +56,7 @@ const ArticlesPage = memo((props: ArticlesPageProps) => {
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
             <Page
+                data-testid="ArticlesPage"
                 onScrollEnd={onLoadNextPart}
                 className={classNames(cls.ArticlesPage, {}, [className])}
             >
